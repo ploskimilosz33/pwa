b589d879345f0808a480a8220da547d4651fda18
ploskimilosz33 <milo.233@wp.pl>
2026-05-09 08:33:57 +0200

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,38 @@
 export default defineNuxtConfig({
-  css: ['bulma/css/bulma.min.css'],
-  compatibilityDate: '2025-01-01'
+  compatibilityDate: '2025-01-01',
+
+  css: [
+    'bulma/css/bulma.min.css'
+  ],
+
+  modules: [
+    '@vite-pwa/nuxt'
+  ],
+
+  pwa: {
+    registerType: 'autoUpdate',
+
+    manifest: {
+      name: 'Gratka PWA',
+      short_name: 'Gratka',
+      description: 'Aplikacja nieruchomości',
+      theme_color: '#001f78',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+
+      icons: [
+        {
+          src: '/icons/pwa-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/pwa-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  }
 })
